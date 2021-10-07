4f32b440daf8ef5653e637469deed31ef7432191
Luke <62490330+Luke20201@users.noreply.github.com>
2021-10-07 12:59:28 +0100

    Update comments

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
-RegisterKeyMapping("stoprecording", "Stop recording your clips", "keyboard", "F11")
-RegisterKeyMapping("startrecording", "Start recording a clip", "keyboard", "f10")      
+RegisterKeyMapping("stoprecording", "Stop recording your clips", "keyboard", "F11") -- F11 is the default key to stop the recording process.
+RegisterKeyMapping("startrecording", "Start recording a clip", "keyboard", "f10")   -- F10 is the default key to start the recording process.
 
 RegisterCommand("stoprecording", function()
 		StopRecordingAndSaveClip()
